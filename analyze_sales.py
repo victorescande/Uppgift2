00826826b3455956a0106a086a889a933a60813e
wrangman <wrangman@gmail.com>
2024-08-20 21:59:11 +0200

    fixed assignment

---
+++ analyze_sales.py
@@ -0,0 +1,36 @@
+import csv
+import os
+import locale
+
+def analyze_sales_data(filename):
+    products = {}
+
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            product = row['Product']
+            sales = float(row['Sales'])
+            
+            if product in products:
+                products[product] += sales
+            else:
+                products[product] = sales
+    
+    #TODO: Hitta den mest sålda produkten (TIPS! Använd Counter från collections)
+    
+    # Hitta den mest lukrativa produkten
+    most_lucrative_product = max(products, key=products.get)
+    
+    # Genomsnittlig försäljning per produkt
+    average_sales = sum(products.values()) / len(products)
+
+    print(f"Mest sålda produkt: ??, Antal: ??")  #FIXME: Redovisa mest sålda produkt här
+    print(f"Mest lukrativa produkt: \"{most_lucrative_product}\" med försäljning på {locale.currency(products[most_lucrative_product],grouping=True)}")
+    print(f"Genomsnittlig försäljning per produkt: {locale.currency(average_sales, grouping=True)}")
+
+
+# Sätt språkinställning till svenska (Sverige) används för att skriva ut formaterad valuta
+locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
+
+os.system('cls')
+analyze_sales_data('sales_data.csv')
